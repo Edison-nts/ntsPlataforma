3c474b2f9279c81e20648ab107be90fd2e8bd1bd
Edison-nts <edison@nts.com.br>
2016-07-27 17:32:58 -0300

    Ajuste em testSite, app.js

--- SiteTeste/js/app.js
+++ SiteTeste/js/app.js
@@ -5,10 +5,19 @@
     app.controller('ContatoController', function ($scope, $http) {
         $scope.hideAlerta = false;
         $scope.result = "";
+        $scope.Mensagem = "Carregando Dados - Aguarde";
 
         $http.get("/api/Contato/Teste").then(function (response) {
-            $scope.hideAlerta = true;
             $scope.Contatos = response.data;
+            if (response.data.length == 0) {
+                $scope.hideAlerta = false;
+                $scope.Mensagem = "Nenhum registro encontrado";
+            }
+            else {
+                $scope.hideAlerta = true;
+            }
+
+            
         });
 
     });
